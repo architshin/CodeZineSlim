90f3410e44e4baee30311987f7e313294d41d136
Shinzo SAITO <architshin@websarva.com>
2019-01-17 18:46:52 +0900

    第2回用サンプルソースの追加。

---
+++ src/public/index.php
@@ -0,0 +1,15 @@
+<?php
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\App as App;
+
+require_once("../vendor/autoload.php");
+
+$app = new App();
+$app->get("/hello",
+	function(Request $request, Response $response, array $args): void
+	{
+		print("<h1>Hello World!</h1>");
+	}
+);
+$app->run();
