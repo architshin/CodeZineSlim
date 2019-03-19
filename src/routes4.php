00d498d00330f8904d0cfe28bdf062f2b1c14006
Shinzo SAITO <architshin@websarva.com>
2019-03-19 16:08:17 +0900

    3個目のサンプル追加。

--- src/routes4.php
+++ src/routes4.php
@@ -20,3 +20,12 @@ $app->any("/no4/helloTwig",
 		return $view->render($response, "hello.html");
 	}
 );
+
+$app->any("/no4/helloWithVals",
+	function(Request $request, Response $response, array $args): Response
+	{
+		$assign["name"] = "夏目";
+		$view = new Twig("../templates");
+		return $view->render($response, "no4/helloWithVals.html", $assign);
+	}
+);
