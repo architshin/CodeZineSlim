5134d5660fa491d9ad2e6fbe9908f49f68ee6338
Shinzo SAITO <architshin@websarva.com>
2019-03-19 15:41:43 +0900

    2個目のサンプル追加。

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,8 @@ use Slim\App as App;
 require_once("../vendor/autoload.php");
 
 $app = new App();
+
 require_once("../routes.php");
 require_once("../routes4.php");
+
 $app->run();
--- src/routes4.php
+++ src/routes4.php
@@ -2,6 +2,7 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\App as App;
+use \Slim\Views\Twig;
 
 $app->any("/no4/getDataByJSON/{id}",
 	function(Request $request, Response $response, array $args): Response
@@ -9,5 +10,13 @@ $app->any("/no4/getDataByJSON/{id}",
 		$id = $args["id"];
 		$jsonArray =["id" => $id, "name" => "田中和彦", "age" => 26];
 		return $response->withJson($jsonArray);
+	}
+);
+
+$app->any("/no4/helloTwig",
+	function(Request $request, Response $response, array $args): Response
+	{
+		$view = new Twig("../templates");
+		return $view->render($response, "hello.html");
 	}
 );
