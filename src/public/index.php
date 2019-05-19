42c93e3e62405f5ab37137cef41e7a828e348086
Shinzo SAITO <architshin@websarva.com>
2019-05-20 01:05:05 +0900

    1個目のサンプル追加。

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,20 @@
 <?php
 use Slim\App as App;
+use \Slim\Views\Twig;
 
 require_once("../vendor/autoload.php");
 
 $app = new App();
 
+$container = $app->getContainer();
+$container["view"] = function($container)
+{
+	$view = new Twig("../templates");
+	return $view;
+};
+
 require_once("../routes.php");
 require_once("../routes4.php");
+require_once("../routes5.php");
 
 $app->run();
---
+++ src/routes5.php
@@ -0,0 +1,26 @@
+<?php
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\App as App;
+
+function getTwigInstance()
+{
+	$view = new Twig("../templates");
+	return $view;
+}
+
+$app->any("/no5/helloTwigWithFunc",
+	function(Request $request, Response $response, array $args): Response
+	{
+		$view = getTwigInstance();
+		return $view->render($response, "hello.html");
+	}
+);
+
+$app->any("/no5/helloTwigWithContainer",
+	function(Request $request, Response $response, array $args): Response
+	{
+		$view = $this->get("view");
+		return $view->render($response, "hello.html");
+	}
+);
