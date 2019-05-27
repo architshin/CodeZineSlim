67836ddbe26f281b584c46dba986f10660709d13
Shinzo SAITO <architshin@websarva.com>
2019-05-27 21:12:14 +0900

    use宣言を修正。

--- src/routes5.php
+++ src/routes5.php
@@ -1,7 +1,7 @@
 <?php
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use Slim\App as App;
+use \Slim\Views\Twig;
 
 function getTwigInstance()
 {
