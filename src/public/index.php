9c9c56e2fd143ae23c67d19bc4d8a0b81efbb03a
Shinzo SAITO <architshin@websarva.com>
2019-12-24 16:04:42 +0900

    第3回までのサンプルを動くように修正。

--- src/public/index.php
+++ src/public/index.php
@@ -1,31 +1,32 @@
 <?php
-use Slim\App as App;
-use Slim\Views\Twig;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
+use Slim\Factory\AppFactory;
+// use Slim\Views\Twig;
+// use Monolog\Logger;
+// use Monolog\Handler\StreamHandler;
+// use Monolog\Handler\FirePHPHandler;
 
 require_once("../vendor/autoload.php");
 
-$app = new App();
+$app = AppFactory::create();
 
-$container = $app->getContainer();
-$container["view"] = function($container)
-{
-	$view = new Twig("../templates");
-	return $view;
-};
-$container["logger"] = function($container)
-{
-	$logger = new Logger("firstslim");
-	$fileHandler = new StreamHandler("../../logs/app.log");
-	$logger->pushHandler($fileHandler);
-	return $logger;
-};
+// $container = $app->getContainer();
+// $container["view"] = function($container)
+// {
+// 	$view = new Twig("../templates");
+// 	return $view;
+// };
+// $container["logger"] = function($container)
+// {
+// 	$logger = new Logger("firstslim");
+// 	$fileHandler = new StreamHandler("../../logs/app.log");
+// 	$logger->pushHandler($fileHandler);
+// 	return $logger;
+// };
 
+$app->setBasePath("/firstslim/src/public");
 require_once("../routes.php");
-require_once("../routes4.php");
-require_once("../routes5.php");
-require_once("../routes6.php");
+// require_once("../routes4.php");
+// require_once("../routes5.php");
+// require_once("../routes6.php");
 
 $app->run();
--- src/routes.php
+++ src/routes.php
@@ -2,57 +2,58 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\App as App;
+use Slim\Routing\RouteCollectorProxy;
 
 $app->get("/hello",
-	function(Request $request, Response $response, array $args): void
-	{
+	function(Request $request, Response $response, array $args): Response {
 		print("<h1>Hello World!</h1>");
+		return $response;
 	}
 );
 $app->post("/helloPost",
-	function(Request $request, Response $response, array $args): void
-	{
+	function(Request $request, Response $response, array $args): Response {
 		print("<h1>POSTメソッドでこんにちは!</h1>");
+		return $response;
 	}
 );
 $app->any("/helloAny",
-	function(Request $request, Response $response, array $args): void
-	{
+	function(Request $request, Response $response, array $args): Response {
 		$method = $request->getMethod();
 		print("<h1>{$method}でメソッドでこんにちは!</h1>");
+		return $response;
 	}
 );
 $app->map(["POST", "GET"], "/helloMap",
-	function(Request $request, Response $response, array $args): void
-	{
+	function(Request $request, Response $response, array $args): Response {
 		print("<h1>POSTまたはGETメソッドでこんにちは!</h1>");
+		return $response;
 	}
 );
 $app->get("/showDetail/{id}",
-	function(Request $request, Response $response, array $args): void
-	{
+	function(Request $request, Response $response, array $args): Response {
 		$id = $args["id"];
 		print("<h1>IDが{$id}の詳細です!</h1>");
+		return $response;
 	}
 );
 
-$app->group("/members", function(App $app): void {
-	$app->any("/showList", 
-		function(Request $request, Response $response, array $args): void
-		{
+$app->group("/members", function(RouteCollectorProxy $group): void {
+	$group->any("/showList", 
+		function(Request $request, Response $response, array $args): Response {
 			print("<h1>/membersグループの/showListです。</h1>");
+			return $response;
 		}
 	);
-	$app->any("/entry", 
-		function(Request $request, Response $response, array $args): void
-		{
+	$group->any("/entry", 
+		function(Request $request, Response $response, array $args): Response {
 			print("<h1>/membersグループの/entryです。</h1>");
+			return $response;
 		}
 	);
-	$app->any("/showDetail", 
-		function(Request $request, Response $response, array $args): void
-		{
+	$group->any("/showDetail", 
+		function(Request $request, Response $response, array $args): Response {
 			print("<h1>/membersグループの/showDetailです。</h1>");
+			return $response;
 		}
 	);
 });
