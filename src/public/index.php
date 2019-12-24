0573feb6d024efba35fd16bcc89e057b5891012d
Shinzo SAITO <architshin@websarva.com>
2019-12-25 02:20:31 +0900

    第5回までのサンプルを動くように修正。

--- src/public/index.php
+++ src/public/index.php
@@ -1,32 +1,31 @@
 <?php
+use DI\Container;
 use Slim\Factory\AppFactory;
-// use Slim\Views\Twig;
-// use Monolog\Logger;
-// use Monolog\Handler\StreamHandler;
-// use Monolog\Handler\FirePHPHandler;
+use Slim\Views\Twig;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
 
 require_once("../vendor/autoload.php");
 
+$container = new Container();
+$container->set("view", function($container) {
+	$view = Twig::create("../templates");
+	return $view;
+});
+$container->set("logger", function($container) {
+	$logger = new Logger("firstslim");
+	$fileHandler = new StreamHandler("../../logs/app.log");
+	$logger->pushHandler($fileHandler);
+	return $logger;
+});
+AppFactory::setContainer($container);
 $app = AppFactory::create();
-
-// $container = $app->getContainer();
-// $container["view"] = function($container)
-// {
-// 	$view = new Twig("../templates");
-// 	return $view;
-// };
-// $container["logger"] = function($container)
-// {
-// 	$logger = new Logger("firstslim");
-// 	$fileHandler = new StreamHandler("../../logs/app.log");
-// 	$logger->pushHandler($fileHandler);
-// 	return $logger;
-// };
 
 $app->setBasePath("/firstslim/src/public");
 require_once("../routes.php");
 require_once("../routes4.php");
-// require_once("../routes5.php");
+require_once("../routes5.php");
 // require_once("../routes6.php");
 
 $app->run();
