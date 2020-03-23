32f9155b55dcca04e1571d4310a5995cdef7ec88
Shinzo SAITO <architshin@websarva.com>
2020-03-23 17:22:28 +0900

    2コめのサンプル追加。

---
+++ src/classes/middlewares/AfterHelloMiddleware.php
@@ -0,0 +1,17 @@
+<?php
+namespace CodeZineSlim\FirstSlim\middlewares;
+
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class AfterHelloMiddleware implements MiddlewareInterface
+{
+	public function process(Request $request, RequestHandlerInterface $handler): Response
+	{
+		$response = $handler->handle($request);
+		print("AfterHelloMidlewareからHello!<br>");
+		return $response;
+	}
+}
--- src/routes8.php
+++ src/routes8.php
@@ -1,5 +1,7 @@
 <?php
 use CodeZineSlim\FirstSlim\controllers\Chap8Controller;
 use CodeZineSlim\FirstSlim\middlewares\BeforeHelloMiddleware;
+use CodeZineSlim\FirstSlim\middlewares\AfterHelloMiddleware;
 
 $app->any("/no8/withBeforeMiddleware", Chap8Controller::class.":mainRequest")->add(new BeforeHelloMiddleware());
+$app->any("/no8/withAfterMiddleware", Chap8Controller::class.":mainRequest")->add(new AfterHelloMiddleware());
